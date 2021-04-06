4f46296aee7c00848272b0ed7b77fc049fce736d
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-05 19:29:51 -0700

    dahsboard api

---
+++ controllers/api/dashboardRoutes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { User, Comment, Post } = require('../../models');
+const withAuth=require('.../utils/auth.js');
+
+router.get('/', withAuth, async (req, res)=>{
+    try{
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id', 'title', 'text_area', 'created_at'
+            ], 
+            include: [
+                {model: Comment,User,  
+                attributes: ['id', 'comment_text', 'user_id', 'created_at', 'email_username']
+            },
+            ],
+        });
+        const posts = postData.map((post)=> post.get({plain: true})
+        );
+        res.render('dashboard', {
+            posts, loggedIn:true});
+        }
+        catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }}
+);
+router.get('/edit/:id', withAuth, async (req, res)=>{
+    try{
+        const postData = await Post.findAll({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id', 'title', 'text_area', 'created_at'
+            ], 
+            include: [
+                {model: Comment,User,  
+                attributes: ['id', 'comment_text', 'user_id', 'created_at', 'email_username']
+            },
+            ],
+        });
+        postData=>{
+            if(!postData){
+                res.status(404).json({message: 'No post found'});
+            }
+        }
+        const post = postData.get({plain: true});
+        res.render('edit-post', {post, loggedIn:true});
+        }
+        catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }}
+);
+
+router.get('new', async (req,res)=>{
+    res.render('new-post');
+});
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes=require('./api/dashboardRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', postRoutes);
 
 module.exports = router;
