e84f88f6a0330ff0086aa69ff6b0b1e68bfb211c
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-20 18:33:24 -0700

    update

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 const sequelize = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, 'public')));
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -26,13 +27,12 @@ const sess = {
 app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
+app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
 app.use(routes);
 
 
