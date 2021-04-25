8475c7655c23f1a5a8465c0b21759b4ac73bfae8
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-25 14:39:27 -0700

    time session

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 120000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
